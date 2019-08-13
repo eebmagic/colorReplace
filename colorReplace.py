a6111950cef492dadb7d8687ecb5a36484c32f6f
Ethan B <eebmagic@gmail.com>
2019-08-13 18:09:50 -0400

    small style changes

--- colorReplace.py
+++ colorReplace.py
@@ -2,9 +2,11 @@ from PIL import Image
 import sys
 from tqdm import tqdm
 import os
+from ast import literal_eval
 
 currentPath = os.popen("pwd").read()[:-1]
 print(currentPath)
+
 
 def listCounts(inputList):
 	outputDict = {}
@@ -30,7 +32,6 @@ def findLargestVals(inputDict):
 				allVals.pop(i)
 				bigs.append(val)
 
-	
 	points = {}
 	for point in inputDict:
 		if inputDict[point] in bigs:
@@ -39,40 +40,41 @@ def findLargestVals(inputDict):
 	return points
 
 
-############################################  START  ############################################
+#  START  #
 
-### Get Input Image ###
+# Get Input Image #
 image_PATH = None
 for item in sys.argv:
 	if item.endswith(".png") or item.endswith(".jpg"):
 		image_PATH = item
 
-if image_PATH == None:
+if image_PATH is None:
 	image_PATH = input("Drag image here: ").strip()
 
 # Get raw data from image #
 im = Image.open(image_PATH)
 raw_data = list(im.getdata())
 
-### Count all Colors in image ###
+# Count all Colors in image #
 counts = listCounts(raw_data)
 mostCommon = findLargestVals(counts)
 
-### Get user Color Selection ###
+# Get user Color Selection #
 print("\nMost common colors are:")
 for i, color in enumerate(mostCommon.keys()):
-	print("  " + str(i) + " - " + str(color[:3]) + " "*(15-len(str(color[:3]))) + "   | " + str(mostCommon[color]))
+	print("  " + str(i) + " - " + str(color[:3]) + " " * (15 - len(str(color[:3]))) + "   | " + str(mostCommon[color]))
 
-userSelection = int(input("\nWhich color would you like to pick to replace? (0-4): "))
+colorSelectPhrase = "\nWhich color would you like to pick to replace? (0-4): "
+userSelection = int(input(colorSelectPhrase))
 userColor = list(mostCommon.keys())[userSelection]
 print(f"You picked: {userColor[:3]}")
 
-### Get replace Color ###
-userNewColor_string = input("Type the color that you want to replace with as (R, G, B): ").strip()
-from ast import literal_eval
+# Get replace Color #
+colorInPhrase = "Type the color that you want to replace with as (R, G, B): "
+userNewColor_string = input(colorInPhrase).strip()
 userNewColor_tuple = literal_eval(userNewColor_string)
 
-### MAKE NEW COLOR DATA ###
+# MAKE NEW COLOR DATA #
 newData = []
 print("Making new Data")
 for pixel in tqdm(raw_data):
@@ -83,9 +85,8 @@ for pixel in tqdm(raw_data):
 # print(newData)
 # quit()
 
-### Send new color data to new image file ###
+# Send new color data to new image file #
 newIm = Image.new("RGB", (im.size[0], im.size[1]))
 print(len(newData), type(newData), type(newData[0]))
 newIm.putdata(newData)
 newIm.save(currentPath + "/output.png")
-
